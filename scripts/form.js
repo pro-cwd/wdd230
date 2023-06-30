a70391d86bb3087ac71d4cf365c88a83ae7f2a08
Omar Meza <proyectos@codicewd.com>
2023-06-29 19:41:40 -0500

    Team Form

--- scripts/form.js
+++ scripts/form.js
@@ -5,23 +5,25 @@
     const kp1 = document.querySelector("#password");
     const kp2 = document.querySelector("#confirmPassword");
     const message = document.querySelector("#formmessage");
-
-kp2.addEventListener("focusout", checkSame);
-
-// This should be refactored.
-function checkSame() {
-	if (kp1.value !== kp2.value) {
-		message.textContent = "❗Key passwords DO NOT MATCH!";
-		message.style.visibility = "show";
-		kp2.style.backgroundColor = "#fff0f3";
-		kp2.value = "";
-		kp2.focus();
-	} else {
-		message.style.display = "none";
-		kp2.style.backgroundColor = "#fff";
-		kp2.style.color = "#000";
-	}
+    const email = document.getElementById("email").value;
+    
+    kp2.addEventListener("focusout", checkSame);
+    
+    // This should be refactored.
+    function checkSame() {
+        if (kp1.value !== kp2.value) {
+            message.textContent = "❗Key passwords DO NOT MATCH!";
+            message.style.visibility = "show";
+            kp2.style.backgroundColor = "#fff0f3";
+            kp2.value = "";
+            kp2.focus();
+        } else {
+            message.style.display = "none";
+            kp2.style.backgroundColor = "#fff";
+            kp2.style.color = "#000";
+        }
 }
+
 
     
     // Update the rating label when the range input value changes
