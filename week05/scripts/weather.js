f9d86be5987638133b4e747dbe06316ef1cc86a3
Omar Meza <proyectos@codicewd.com>
2023-07-06 18:24:57 -0500

    APP weather

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,14 +1,15 @@
-const currentTemp = document.querySelector('current-temp');
-const weatherIcon = document.querySelector('weather-icon');
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.63&appid=1ce02409f68cb2fb2a0f889799ee056a'
 
-async function apiFetch() {
+async function apiFetch(url) {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            //console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -17,13 +18,14 @@ async function apiFetch() {
         console.log(error);
     }
 }
-apiFetch();
+apiFetch(url);
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.____}&deg;F`
-    const iconsrc = `https://openweathermap.org/img/wn/10d@01d.png`;
+    currentTemp.innerHTML = `${data.temp}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0]}.icon`;
     let desc = data.weather[0].description
-    weatherIcon.setAttribute('src', data.iconsrc)
-    weatherIcon.setAttribute('alt', 'Trier, Germany')
-    cactionDesc.textContent = `${desc}`
+    weatherIcon.setAttribute('src', iconsrc)
+    weatherIcon.setAttribute('alt', 'clear sky')
+    captionDesc.textContent = `${desc}`
 }
+
