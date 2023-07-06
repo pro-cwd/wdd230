85b6c1504d8eb63f1cbab21640065027f92e2bb1
Omar Meza <proyectos@codicewd.com>
2023-07-06 17:18:03 -0500

    weather API

---
+++ week05/scripts/weather.js
@@ -0,0 +1,29 @@
+const currentTemp = document.querySelector('current-temp');
+const weatherIcon = document.querySelector('weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.63&appid=1ce02409f68cb2fb2a0f889799ee056a'
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        } else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.____}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/wn/10d@01d.png`;
+    let desc = data.weather[0].description
+    weatherIcon.setAttribute('src', data.iconsrc)
+    weatherIcon.setAttribute('alt', 'Trier, Germany')
+    cactionDesc.textContent = `${desc}`
+}
