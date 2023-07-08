28801dd847527142f1a353ce2290fa221d07ef44
Omar Meza <proyectos@codicewd.com>
2023-07-08 18:48:26 -0500

    Members directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,8 +4,8 @@ const cards = document.querySelector('#cards');
 async function getMembersData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
-    displayMembers(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+    //console.table(data.members);
+    displayMembers(data.members); // note that we reference the prophets array of the JSON data object, not just the object
 }
 
 getMembersData(url);
@@ -14,11 +14,11 @@ const displayMembers = (members) => {
     members.forEach((member) => {
     // Create elements to add to the article.cards element
     let card = document.createElement('section');
-    let name = document.createElement('h2');
-    let address = document.createElement('h3');
-    let phone = document.createElement('h3');
-    let website = document.createElement('h3');
-    let slogan = document.createElement('h2');
+    let name = document.createElement('h3');
+    let address = document.createElement('h5');
+    let phone = document.createElement('h5');
+    let website = document.createElement('h5');
+    let slogan = document.createElement('h3');
     let logo = document.createElement('img');
 
     name.textContent = `${member.name}`;
@@ -33,12 +33,12 @@ const displayMembers = (members) => {
     logo.setAttribute('height', '340');
 
     // Append the section(card) with the created elements
+    card.appendChild(logo);
     card.appendChild(name);
+    card.appendChild(slogan);
     card.appendChild(address);
     card.appendChild(phone);
     card.appendChild(website);
-    card.appendChild(slogan);
-    card.appendChild(logo);
 
     cards.appendChild(card);
   }); // end of arrow function and forEach loop
