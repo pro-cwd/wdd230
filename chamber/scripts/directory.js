c9bd1b318b5dbbe3324b84f3bd97cac85dcaabe8
Omar Meza <proyectos@codicewd.com>
2023-07-08 16:39:23 -0500

    Dinamics links

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,42 +1,44 @@
-const url = 'https://pro-cwd.github.io/wdd230/chamber/data/members-directoy.json';
+const url = 'https://pro-cwd.github.io/wdd230/chamber/data/members-directory.json';
 const cards = document.querySelector('#cards');
 
-async function getProphetData(url) {
+async function getMembersData(url) {
     const response = await fetch(url);
     const data = await response.json();
     //console.table(data.prophets);
-    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+    displayMembers(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
 }
 
-getProphetData(url);
+getMembersData(url);
 
-const displayProphets = (members) => {
-  members.forEach((member) => {
-    // Create elements to add to the div.cards element
+const displayMembers = (members) => {
+    members.forEach((member) => {
+    // Create elements to add to the article.cards element
     let card = document.createElement('section');
-    let fullName = document.createElement('h2');
-    let birthDays = document.createElement('h3');
-    let birthPlace = document.createElement('h3');
-    let portrait = document.createElement('img');
+    let name = document.createElement('h2');
+    let address = document.createElement('h3');
+    let phone = document.createElement('h3');
+    let website = document.createElement('h3');
+    let slogan = document.createElement('h2');
+    let logo = document.createElement('img');
 
-    // Build the h2 content out to show the prophet's full name
-    fullName.textContent = `${member.name} ${member.lastname}`;
-    //Build the h3 content out to show the birthdays
-    birthDays.textContent = `Date of Birth: ${member.birthdate}`;
-    //Build the h3 content out to show the place both
-    birthPlace.textContent = `Place of Birth: ${member.birthplace}`;
-    // Build the image portrait by setting all the relevant attributes
-    portrait.setAttribute('src', member.imageurl);
-    portrait.setAttribute('alt', `logotype of ${member.name}`);
-    portrait.setAttribute('loading', 'lazy');
-    portrait.setAttribute('width', '340');
-    portrait.setAttribute('height', '440');
+    name.textContent = `${member.name}`;
+    address.textContent = `Address: ${member.address}`;
+    phone.textContent = `Phone: ${member.phone}`;
+    website.textContent = `${member.website}`;
+    slogan.textContent = `${member.slogan}`;
+    logo.setAttribute('src', member.image);
+    logo.setAttribute('alt', `logotype of ${member.name}`);
+    logo.setAttribute('loading', 'lazy');
+    logo.setAttribute('width', '440');
+    logo.setAttribute('height', '340');
 
     // Append the section(card) with the created elements
-    card.appendChild(fullName);
-    card.appendChild(birthDays);
-    card.appendChild(birthPlace);
-    card.appendChild(portrait);
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+    card.appendChild(slogan);
+    card.appendChild(logo);
 
     cards.appendChild(card);
   }); // end of arrow function and forEach loop
--- scripts/links.js
+++ scripts/links.js
@@ -2,48 +2,76 @@ const baseURL = "https://pro-cwd.github.io/wdd230/";
 const linksURL = "https://pro-cwd.github.io/wdd230/data/links.json";
 const links = document.querySelector('#links');
 
+async function getLinksJson() {
+  const request = await fetch(linksURL);
+  const response = await request.json();
 
-async function getLinks(linksURL) {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    //console.log(data);
-    displayLinks(data);
-  }
+  return response;
+}
+
+
+async function main () {
+  const links = await getLinksJson();
+  const weeks = links.weeks;
+
+  let template = '';
+
+  weeks.forEach(week => {
+    let templateLinks = "";
+    week.links.forEach(link => {
+      templateLinks += `<a href="${link.url}">${link.title}</a>`
+    })
+
+    template += `<li><h4><strong>${week.week}:</strong></h4>${templateLinks}</li>`
+  })
+
+  document.querySelector('#links').innerHTML = template
+}
+
+main();
+
+
+// async function getLinks(linksURL) {
+//     const response = await fetch(linksURL);
+//     const data = await response.json();
+//     //console.log(data);
+//     displayLinks(data);
+//   }
   
-  getLinks(linksURL);
+//   getLinks(linksURL);
 
-  const displayLinks = (data) => {
-    Object.keys(data).forEach(weeks => {
-      // Create elements to add to the div.cards element
-        let li = document.createElement('li');
-        let anchor = document.createElement('a');
-        let week = document.createElement('h4');
-        let title = document.createElement('h4');
+//   const displayLinks = (data) => {
+//     Object.keys(data).forEach(weeks => {
+//       // Create elements to add to the div.cards element
+//         let li = document.createElement('li');
+//         let anchor = document.createElement('a');
+//         let week = document.createElement('h4');
+//         let title = document.createElement('h4');
         
-        let liTav = [];
-        // Iterate over each week object in the weeks array
-        for (var i = 0; i < data.weeks.length; i++) {
-          let wee = data.weeks[i];
-          week.textContent = `${wee.week}: `;
+//         let liTav = [];
+//         // Iterate over each week object in the weeks array
+//         for (var i = 0; i < data.weeks.length; i++) {
+//           let wee = data.weeks[i];
+//           week.textContent = `${wee.week}: `;
 
-          li.appendChild(week);
-          // Generate the HTML for the links within the week
-          for (var j = 0; j < wee.links.length; j++) {
-            let link = wee.links[j];
-            let lnk = link.url;
+//           li.appendChild(week);
+//           // Generate the HTML for the links within the week
+//           for (var j = 0; j < wee.links.length; j++) {
+//             let link = wee.links[j];
+//             let lnk = link.url;
             
-            title.textContent = ` ${link.title} |`;
-            anchor.setAttribute('href', lnk);
-            li.appendChild(anchor);
-            anchor.appendChild(title);
-          }
+//             title.textContent = ` ${link.title} |`;
+//             anchor.setAttribute('href', lnk);
+//             li.appendChild(anchor);
+//             anchor.appendChild(title);
+//           }
           
-          links.appendChild(li);
+//           links.appendChild(li);
 
-        }
+//         }
       
-    }); // end of arrow function and forEach loop
-  }
+//     }); // end of arrow function and forEach loop
+//   }
 
 
 
