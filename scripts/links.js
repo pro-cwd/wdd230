d44e0da45ee1b090a38640be1fdbabe584caa5bc
Omar Meza <proyectos@codicewd.com>
2023-07-07 20:39:04 -0500

    dinamicly link

--- scripts/links.js
+++ scripts/links.js
@@ -19,33 +19,29 @@ async function getLinks(linksURL) {
         let anchor = document.createElement('a');
         let week = document.createElement('h4');
         let title = document.createElement('h4');
-        let icon = data.weeks[0].links[0].url;
-
-        week.textContent = `${data.weeks[0].week}: `;
-        title.textContent = ` ${data.weeks[0].links[0].title}: |`;
-        anchor.setAttribute('href', icon);
-  
+        
+        let liTav = [];
         // Iterate over each week object in the weeks array
         for (var i = 0; i < data.weeks.length; i++) {
           let wee = data.weeks[i];
           week.textContent = `${wee.week}: `;
-          // Generate the HTML for the week heading
+
           li.appendChild(week);
-          
           // Generate the HTML for the links within the week
           for (var j = 0; j < wee.links.length; j++) {
             let link = wee.links[j];
             let lnk = link.url;
-            console.log(link)
             
             title.textContent = ` ${link.title} |`;
             anchor.setAttribute('href', lnk);
             li.appendChild(anchor);
             anchor.appendChild(title);
           }
+          
+          links.appendChild(li);
+
         }
-        links.appendChild(li);
-  
+      
     }); // end of arrow function and forEach loop
   }
 
