44683166106399de5d02327a743445fd058024c4
Omar Meza <proyectos@codicewd.com>
2023-07-13 00:10:18 -0500

    home page enhansement

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,6 +1,6 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const captionDesc = document.querySelector('#description');
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.63&units=imperial&appid=1ce02409f68cb2fb2a0f889799ee056a'
 
 async function apiFetch() {
@@ -29,4 +29,60 @@ function displayResults(data) {
     weatherIcon.setAttribute('alt', 'clear sky')
     captionDesc.textContent = `${desc}`
 }
+
+// weather forecast
+window.addEventListener('DOMContentLoaded', () => {
+    const apiKey = '1ce02409f68cb2fb2a0f889799ee056a';
+    const lat = '-0.9333';
+    const lon = '-79.2167';
+  
+    const forecastContainer = document.getElementById('forecast-container');
+  
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}&cnt=1&*3`)
+        .then(response => response.json())
+        .then(data => {
+        const forecastList = data.list;
+        const forecastDays = {};
+  
+        // Group forecast data by day
+        forecastList.forEach(forecast => {
+            const date = new Date(forecast.dt_txt);
+            const day = date.toLocaleDateString('es-EC', { weekday: 'long' });
+            console.log(day)
+  
+            if (!forecastDays[day]) {
+            forecastDays[day] = [];
+            }
+  
+            forecastDays[day].push(forecast);
+        });
+  
+        // Display forecast for each day
+        Object.entries(forecastDays).forEach(([day, forecasts]) => {
+
+            const forecastDay = document.createElement('div');
+            forecastDay.className = 'forecast-day';
+  
+            const heading = document.createElement('h4');
+            heading.textContent = day;
+            forecastDay.appendChild(heading);
+
+            forecasts.forEach(forecast => {
+            const temperature = forecast.main.temp.toFixed(1);
+            const description = forecast.weather[0].description;
+                
+            const forecastInfo = document.createElement('p');
+            forecastInfo.textContent = `${temperature}°C - ${description}`;
+  
+            forecastDay.appendChild(forecastInfo);
+            });
+                forecastContainer.appendChild(forecastDay);
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        forecastContainer.textContent = 'An error occurred while fetching the forecast.';
+      });
+  });
+  
 
