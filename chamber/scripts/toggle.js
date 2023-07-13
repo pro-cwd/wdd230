3192df80502af8aa94ccfdf211934ec49417d781
Omar Meza <proyectos@codicewd.com>
2023-07-12 19:09:54 -0500

    correct issue directory toggle

--- chamber/scripts/toggle.js
+++ chamber/scripts/toggle.js
@@ -1,16 +1,16 @@
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+const display = document.querySelector(".article");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
-gridbutton.addEventListener("click", () => {
+gridButton.addEventListener("click", () => {
 	// example using arrow function
 	display.classList.add("grid");
 	display.classList.remove("list");
 });
 
-listbutton.addEventListener("click", showList); // example using defined function
+listButton.addEventListener("click", showList); // example using defined function
 
 function showList() {
 	display.classList.add("list");
---
+++ chamber/scripts/weather.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.63&units=imperial&appid=1ce02409f68cb2fb2a0f889799ee056a'
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            //console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`
+    let desc = data.weather[0].description
+    let icon = data.weather[0].icon
+    const iconsrc = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    weatherIcon.setAttribute('src', iconsrc)
+    weatherIcon.setAttribute('alt', 'clear sky')
+    captionDesc.textContent = `${desc}`
+}
+
